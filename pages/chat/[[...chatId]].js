a1be9c4818afc48e506066f5c95785bf2680cb7e
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 15:08:30 +0530

    Setting up chat route page

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CatGPT</title>
+      </Head>
+      <h1>Welcome to the chat Id Page</h1>
+    </div>
+  );
+}
