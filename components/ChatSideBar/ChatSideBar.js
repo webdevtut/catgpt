5ec9f791f3348769662c4e1e5e502aba11767193
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 18:48:01 +0530

    Create ChatSidebar component & add logout link

---
+++ components/ChatSideBar/ChatSideBar.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export const ChatSideBar = () => {
+    return <div className="bg-gray-900 text-white">
+        <Link href="/api/auth/logout">Logout</Link>
+    </div>;
+};
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,3 +1,4 @@
+import { ChatSideBar } from "components/ChatSideBar";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
         <title>CatGPT | New Chat</title>
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
-        <div>Sidebar</div>
+        <ChatSideBar />
         <div className="bg-gray-700 flex flex-col">
           <div className="flex-1">chat window</div>
           <footer className="bg-gray-800 p-10">footer</footer>
