8c7cde09d5e367348f1c3baafe773e0d31eae31a
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 16:51:52 +0530

    Set up the login and logout state

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 export default function Home() {
+  const {isLoading, error, user} = useUser();
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div>
       <Head>
@@ -9,7 +15,8 @@ export default function Home() {
       </Head>
       <h1>Welcome to the Next JS &amp; CatGPT</h1>
       <div>
-        <Link href="/api/auth/login">Login</Link>
+        {!!user && <Link href="/api/auth/logout">Logout</Link>}
+        {!user && <Link href="/api/auth/login">Login</Link>}
       </div>
     </div>
   );
