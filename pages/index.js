f8663c4d6657b3dcd1391b96d0927477dc657e2a
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 14:56:29 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CatGPT</title>
+      </Head>
+      <h1>Welcome to the Next JS &amp; CatGPT</h1>
+    </div>
+  );
+}
