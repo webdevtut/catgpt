6d3f6c88310335e80b5e8a17ba8a883f7dd51431
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 19:13:12 +0530

    footer & message form

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ export default function Home() {
         {!!user && <Link href="/api/auth/logout">Logout</Link>}
         {!user && (
             <>
-            <Link href="/api/auth/login" className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600">Login</Link>
-            <Link href="/api/auth/signup" className="rounded-md bg-emerald-500 ml-2 px-4 py-2 text-white hover:bg-emerald-600">Signup</Link>
+            <Link href="/api/auth/login" className="btn">Login</Link>
+            <Link href="/api/auth/signup" className="btn ml-2">Signup</Link>
             </>
             )}
         </div>
