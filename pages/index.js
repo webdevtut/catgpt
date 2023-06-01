c4689789e122ffe7a6579be8f26d6d36a7bfb92b
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-01 16:13:05 +0530

    create auth api routes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,6 +8,9 @@ export default function Home() {
         <title>CatGPT</title>
       </Head>
       <h1>Welcome to the Next JS &amp; CatGPT</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
     </div>
   );
 }
