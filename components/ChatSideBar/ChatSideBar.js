f169dc7b063519d25ccccbd1588ea441f472d506
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-03 11:55:58 +0530

    getChats endpoint

--- components/ChatSideBar/ChatSideBar.js
+++ components/ChatSideBar/ChatSideBar.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 export const ChatSideBar = () => {
+    useEffect(() => {
+      const loadChatList = async () => {
+        const response = await fetch(`/api/chat/getChatList`, {
+          method: "POST",
+        });
+        const json = await response.json();
+        console.log("Chat List", json);
+      };
+      loadChatList();
+    });
     return <div className="bg-gray-900 text-white">
         <Link href="/api/auth/logout">Logout</Link>
     </div>;
