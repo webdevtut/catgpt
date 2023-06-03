e0bba81659be02ee7b3304090f06745c22f01743
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-06-03 11:38:16 +0530

    Create and Test createChat endpoint

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -22,23 +22,34 @@ export default function ChatPage() {
       return newChatMessages;
     })
     setMessageText("");
-    const response = await fetch(`/api/chat/sendMessage`, {
-      method: "POST",
+    const response = await fetch(`/api/chat/createNewChat`, {
+            method: "POST",
       headers: {
         "content-type": "application/json",
       },
       body: JSON.stringify({
-        message: messageText,
-      }),
+            message: messageText,
+          }),
     });
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-    const reader = data.getReader();
-    await streamReader(reader, (message) => {
-      setIncomingMessage((s) => `${s}${message.content}`);
-    });
+    const json = await response.json();
+    console.log("New Chat: ", json);
+    // const response = await fetch(`/api/chat/sendMessage`, {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     message: messageText,
+    //   }),
+    // });
+    // const data = response.body;
+    // if (!data) {
+    //   return;
+    // }
+    // const reader = data.getReader();
+    // await streamReader(reader, (message) => {
+    //   setIncomingMessage((s) => `${s}${message.content}`);
+    // });
     setGeneratingResponse(false);
   };
   return (
